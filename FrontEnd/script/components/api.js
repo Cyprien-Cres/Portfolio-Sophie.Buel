8e54daa7b6268f380da61d153d409d8ed17ce9e7
Cyprien <cres.cyprien@gmail.com>
2023-10-28 17:42:46 +0200

    IW-70 Object-fit sur les images et factorisation

--- FrontEnd/script/components/api.js
+++ FrontEnd/script/components/api.js
@@ -1,9 +1,17 @@
 const WORKS_URL = 'http://localhost:5678/api/works'
 const CATEGORIES_URL = 'http://localhost:5678/api/categories'
+const LOGIN_URL = 'http://localhost:5678/api/users/login'
 
 const get = async url => fetch(url).then(res => res.json()).then(data => data).catch(error => error)
 
-// TODO créer une méthode post
+const post = async (url, data) => fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+        "Content-Type": "application/json"
+    }
+}).then(res => res.json()).then(data => data).catch(error => error)
+
 
 export const getWorks = async categoryId => await get(WORKS_URL).then(data => {
     const dataFiltered = categoryId ? data.filter(item => item.categoryId === categoryId) : data
@@ -11,3 +19,12 @@ export const getWorks = async categoryId => await get(WORKS_URL).then(data => {
 })
 
 export const getCategories = async () => await get(CATEGORIES_URL)
+
+export const postLogin = async data => await post(LOGIN_URL, data)
+
+export const deleteProject = async id => await fetch(`http://localhost:5678/api/works/${id}`, {
+    method: 'DELETE',
+    headers: {
+        Authorization: "Bearer " + localStorage.token,
+    }
+})
